e346483e9a6be92f4f199968254788a48807cc6d
danielvdg <daniel.vdg@hotmail.com>
2021-07-26 18:50:53 -0300

    porta pelo env

--- servidor.js
+++ servidor.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-
+const port = 3000
 const bd = require('./src/data/sqlite-bd')
 
 const app = express()
-const port = process.env.PORT
+// const port = process.env.PORT
 
 const dogsController =require('./src/controllers/dogsControllers')
 
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 dogsController(app,bd)
-app.listen(port, ()=> console.log(`[INFO] Servidor rodando na porta: ${port}
+app.listen(process.env.PORT, ()=> console.log(`[INFO] Servidor rodando na porta: ${port}
 Acessar http://localhost:${port}`)
 
 );
